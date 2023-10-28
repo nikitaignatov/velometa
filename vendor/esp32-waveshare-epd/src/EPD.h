36bee596252f8a4149cc6ef4e033cddb95edc0ea
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2023-10-28 17:10:57 +0200

    driver deps

---
+++ vendor/esp32-waveshare-epd/src/EPD.h
@@ -0,0 +1,11 @@
+#ifndef __EPD_H_ 
+#define __EPD_H_ 
+
+#include "utility/Debug.h"
+#include "utility/EPD_2in9.h"
+#include "utility/EPD_4in2.h"
+#include "utility/EPD_4in2_V2.h"
+#include "utility/EPD_4in2bc.h"
+#include "utility/EPD_4in2b_V2.h"
+
+#endif
