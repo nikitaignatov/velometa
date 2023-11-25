db6360b16ff12e674d14fae13b02014af351a171
Nikita Ignatov <nikita.ignatov@hotmail.com>
2023-11-25 21:46:09 +0100

    added lvgl

---
+++ lib/drivers/lcd/wt32sc01plus/wt32sc01plus.hpp
@@ -0,0 +1,12 @@
+
+#define LV_CONF_INCLUDE_SIMPLE
+#define LV_USE_THEME_NIGHT 1
+/* Uncomment below line to draw on screen with touch */
+#define DRAW_ON_SCREEN
+
+#include "lgfx.hpp"
+
+static LGFX lcd; // declare display variable
+
+#include <lvgl.h>
+#include "lv_conf.h"
